e0771cc2d4a6a71d109956af9cc89acbb440ae50
Vitor Bahir <vitorb82014@gmail.com>
2023-03-24 13:07:37 -0300

    A better implementation

--- Program.cs
+++ Program.cs
@@ -4,17 +4,17 @@
     {
         string path = @"c:\temp\file1.txt";
 
-        FileStream fs = null;
         StreamReader sr = null;
 
         try
         {
-            fs = new FileStream(path, FileMode.Open);
-            sr = new StreamReader(fs);
+            sr = File.OpenText(path);
 
-            string line = sr.ReadLine();
-
-            Console.WriteLine(line);
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
         }
         catch (IOException e)
         {
@@ -24,7 +24,6 @@
         finally
         {
             if (sr != null) sr.Close();
-            if (fs != null) fs.Close();
         }
 
 
